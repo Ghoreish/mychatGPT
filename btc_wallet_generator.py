9a096adb77c377a1a7b6a3fdf60c535f8afaeb1a
Ali <46873759+Ghoreish@users.noreply.github.com>
2023-03-07 14:05:16 +0330

    just a small step
    it's just a small step for btc wallet cracking.

---
+++ btc_wallet_generator.py
@@ -0,0 +1,16 @@
+import os
+import binascii
+from mnemonic import Mnemonic
+from bip32utils import BIP32Key
+
+# Generate a random 256-bit seed
+seed = os.urandom(32)
+# Convert the seed to a BIP39 mnemonic phrase
+mnemonic = Mnemonic("english").to_mnemonic(seed)
+print('Mnemonic:', mnemonic)
+
+# Create a hierarchical deterministic wallet from the seed
+wallet = BIP32Key.fromEntropy(seed)
+# Derive the first receiving address from the wallet
+address = wallet.ChildKey(0).ChildKey(0).Address()
+print('Address:', address)
